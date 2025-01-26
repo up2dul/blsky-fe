3a4efae3c746f43e64c06f6592bf504865265f83
abdul <up2dul@gmail.com>
2025-01-27 03:55:14 +0700

    feat(page): loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,13 @@ import { socket } from "@/socket";
 import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isLoadingMessages, setIsLoadingMessages] = useState(true);
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState<Message[]>([]);
 
   useEffect(() => {
     socket.on("messagesHistory", (messages: Message[]) => {
+      setIsLoadingMessages(false);
       setMessages(messages);
     });
     return () => {
@@ -29,12 +31,17 @@ export default function Home() {
   return (
     <main className="relative w-full min-h-screen mx-auto bg-slate-950 sm:w-3/4 lg:w-1/2">
       <ul className="h-svh relative overflow-y-auto p-3 pb-[70px] space-y-3">
-        {messages.length === 0 && (
-          <li className="mt-6 text-center text-sm text-slate-500">
+        {isLoadingMessages && (
+          <li className="mt-6 text-center text-slate-400">
+            Loading messages...
+          </li>
+        )}
+        {!isLoadingMessages && messages.length === 0 && (
+          <li className="mt-6 text-center text-slate-400">
             No messages yet. Send a message to start.
           </li>
         )}
-        {messages.map(msg => (
+        {messages.map((msg) => (
           <li
             key={msg.id}
             className="rounded-sm bg-slate-800 p-2 text-slate-50"
@@ -46,14 +53,18 @@ export default function Home() {
           </li>
         ))}
       </ul>
+
       <form
         onSubmit={sendMessage}
-        className="flex gap-2 p-2 absolute bottom-0 w-full bg-slate-800 rounded-t-sm"
+        className="flex gap-2 p-2 pb-3 absolute bottom-0 w-full bg-slate-800 rounded-t-sm border-t-4 border-slate-950"
       >
         <Input
           value={message}
-          placeholder="Type a message here..."
-          onChange={e => setMessage(e.target.value)}
+          placeholder={
+            isLoadingMessages ? "Loading..." : "Type a message here..."
+          }
+          onChange={(e) => setMessage(e.target.value)}
+          disabled={isLoadingMessages}
         />
         <Button type="submit" disabled={message.length < 1}>
           Send
