3f1b3a6f93b49e2bc0ce922b9ec4f571f76d8c45
abdul <up2dul@gmail.com>
2025-01-27 10:41:42 +0700

    fix(action): return type of Message[]

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,9 +1,10 @@
 "use server";
 
 import { db } from "@/lib/db";
+import type { Message } from "@/lib/types";
 
 export async function getMessages() {
-  return await db.selectFrom("messages").selectAll().execute();
+  return await db.selectFrom("messages").selectAll().execute() as Message[];
 }
 
 export async function addMessage(message: string) {
