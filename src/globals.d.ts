00153671b8b16d6ec748550fc09fdd2393d108d7
abdul <up2dul@gmail.com>
2025-01-27 10:37:06 +0700

    fix: ignore lint correctness

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,4 +1,6 @@
+// biome-ignore lint/correctness/noUnusedVariables: it is a global declaration
 namespace NodeJS {
+  // biome-ignore lint/correctness/noUnusedVariables: it is a global declaration
   interface ProcessEnv {
     DATABASE_URL: string;
   }
